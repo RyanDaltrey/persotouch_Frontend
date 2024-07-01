2022e51a8420ca52815fcf681520e0cc663f13c5
Ryan <ryandaltrey@hotmail.com>
2024-07-01 16:20:51 +0200

    ADD installation de react-router-dom et import dans App.js pour pouvoir récupérer le contenu de la page en fonction de l'URL.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
+
+function App() {
+    return <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+        <Route path ="/" element={<AccueilPage/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+}
+
+export default App;
