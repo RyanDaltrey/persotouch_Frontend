a28ec904528fc13a532c1799174c5351da86bc75
Ryan <ryandaltrey@hotmail.com>
2024-07-02 13:24:03 +0200

    ADD css sur la page inscription, display flex tu titre, texte et inputs d'inscription.

--- src/Pages/InscriptionPage.jsx
+++ src/Pages/InscriptionPage.jsx
@@ -2,7 +2,7 @@ import instance from "../API/axios";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 import '../Styles/Inscription.css'
-
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 
 const { useState } = require("react");
 const { useNavigate } = require("react-router-dom")
@@ -43,20 +43,24 @@ const InscriptionPage = () =>{
         <NavBar/>
         <div className="titre_page_inscription">
             <h3>Créer un compte!</h3>
-            </div>
+        </div>
         <div className="texte_accroche_page_inscription">
             <p className="texte_inscription">Le compte PersoTouch vous permet de gagner du temps au moment du paiement, d'accéder à votre panier à partir de n'importe quel appareil et de consulter l'historique de vos commandes.</p>
         </div>
+        <div className="deja_un_compte">
+            <p>Vous avez deja un compte? Vous connecter </p> <ArrowForwardIosOutlinedIcon/>
+        </div>
+
         <div className="block_inscription">
-            <button onClick={() => {register()}}>Inscription</button>
-            <input type="text" name="prenom" defaultValue={prenom} placeholder="prenom" onChange={(e) => {setPrenom(e.target.value)}}/>
-            <input type="text" name="nom" defaultValue={nom} placeholder="nom" onChange={(e) => {setNom(e.target.value)}}/>
-            <input type="email" name="email" defaultValue={email} placeholder="email" onChange={(e) => {setEmail(e.target.value)}}/>
-            <input type="password" name="mot de passe" defaultValue={mdp} placeholder="mot de passe" onChange={(e) => {setMdp(e.target.value)}}/>
-            <input type="text" name="adresse" defaultValue={adresse} placeholder="adresse" onChange={(e) => {setAdresse(e.target.value)}}/>
-            <input type="text" name="ville" defaultValue={ville} placeholder="ville" onChange={(e) => {setVille(e.target.value)}}/>
-            <input type="text" name="code_postal" defaultValue={code_postal} placeholder="CP" onChange={(e) => {setCode_Postal(e.target.value)}}/>
-            <input type="text" name="pays" defaultValue={pays} placeholder="pays" onChange={(e) => {setPays(e.target.value)}}/>
+            <input type="text" name="prenom" defaultValue={prenom} placeholder="prenom" onChange={(e) => {setPrenom(e.target.value)}}className="input_inscription" />
+            <input type="text" name="nom" defaultValue={nom} placeholder="nom" onChange={(e) => {setNom(e.target.value)}} className="input_inscription"/>
+            <input type="email" name="email" defaultValue={email} placeholder="email" onChange={(e) => {setEmail(e.target.value)}} className="input_inscription"/>
+            <input type="password" name="mot de passe" defaultValue={mdp} placeholder="mot de passe" onChange={(e) => {setMdp(e.target.value)}} className="input_inscription"/>
+            <input type="text" name="adresse" defaultValue={adresse} placeholder="adresse" onChange={(e) => {setAdresse(e.target.value)}} className="input_inscription"/>
+            <input type="text" name="ville" defaultValue={ville} placeholder="ville" onChange={(e) => {setVille(e.target.value)}} className="input_inscription"/>
+            <input type="text" name="code_postal" defaultValue={code_postal} placeholder="CP" onChange={(e) => {setCode_Postal(e.target.value)}} className="input_inscription"/>
+            <input type="text" name="pays" defaultValue={pays} placeholder="pays" onChange={(e) => {setPays(e.target.value)}}className="input_inscription"/>
+            <button onClick={() => {register()}} className="boutton_inscription">Inscription</button>
         </div>
         <Footer/>
     </>
