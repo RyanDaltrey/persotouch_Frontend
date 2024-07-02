aa8ff9336f31dc78225fd151875f8b0f122072db
Ryan <ryandaltrey@hotmail.com>
2024-07-02 23:18:31 +0200

    ADD static pour getProduitsById

--- src/Services/produitsService.js
+++ src/Services/produitsService.js
@@ -4,5 +4,9 @@ class produitsService {
     static getAllProduits(){
         return axios.get("http://127.0.0.1:3000/produits");
     }
+
+    static getProduitById(id){
+        return axios.get("http://127.0.0.1:3000/produits/" + id);
+    }
 }
 export default produitsService;
